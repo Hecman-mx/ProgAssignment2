de5b09bbbd2b3d659b3d778e25dd30e3e8ae246c
Hecman-mx <hmcbmx@yahoo.com.mx>
2015-01-25 16:08:33 -0600

    update

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,22 @@
 ## The function makeCacheMatrix generates a matrix and create a cache for its inverse
-
 makeCacheMatrix <- function(x = matrix()) {
-  
   ## Declarando y asignando variables
   matriz_inv <- NULL
-  
   set <- function(y) {
     matriz <<- y
     matriz_inv <<- NULL
   }
-  
   ## generando la matriz inversa y almacenandola en cache
   get <- function() matriz
   set.inverse <- function(solve) matriz_inv <<- solve
   get.inverse <- function() matriz_inv
-  
   list(
     set = set, 
     get = get,
     set.inverse = set.inverse,
     get.inverse = get.inverse)
-  
 }
-
-
 ## calcula la matriz inversa de la funcion makeCacheMatrix()
-
 cacheSolve <- function(matriz_cache, ...) {
   matriz_inv <- matriz_cache$get.inverse()
   # Obtiene el valor de la matriz en caso de que exista
@@ -37,6 +28,5 @@ cacheSolve <- function(matriz_cache, ...) {
   matriz_inv <- solve(matrix.to.inverse)
   matriz_cache$set.inverse(matriz_inv)
   matriz_inv
-  
 }
 
